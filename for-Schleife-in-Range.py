2ad34159c58b4271b1bb5ccd6ea1919b13240020
Lukas Urwyler <lukas.urwyler@swisstopo.ch>
2026-06-16 09:21:50 +0200

    ain

---
+++ for-Schleife-in-Range.py
@@ -0,0 +1,8 @@
+for zahl in range(10):
+    if zahl % 2 == 0 and zahl != 0:
+        print(zahl, "jeah, gerade Zahl")
+    else:
+        print(zahl, "eine andere Zahl")
+    if zahl == 0:
+        print(zahl, "Null")
+    
--- schachbrett.py
+++ schachbrett.py
@@ -5,6 +5,7 @@ numbers = range(8, 1, -1)
 print(abc)
 
 
-while i in range(8, 1, -1):
-    for z in abc[0:8]:
+
+while i in range(-8, 1):
+    for z in abc:
         print(z + str(i) + " " + z + str((i-1)) + " " + z + str((i-2)) + " " + z + str((i-3)) + " " + z + str((i-4)) + " " + z + str((i-5)) + " " + z + str((i-6)) + " " + z + str((i-7)))
