6abde088cc6e316d9fb0c64ae572adfdae64af51
Lukas Urwyler <lukas.urwyler@swisstopo.ch>
2026-06-16 09:40:09 +0200

    ain

--- schachbrett.py
+++ schachbrett.py
@@ -1,11 +1,8 @@
 import string
 
-abc = string.ascii_lowercase
-numbers = range(8, 1, -1)
-print(abc)
+abc = string.ascii_lowercase[:8]
 
-
-
-while i in range(-8, 1):
+for i in range(8, 1, -1):
     for z in abc:
         print(z + str(i) + " " + z + str((i-1)) + " " + z + str((i-2)) + " " + z + str((i-3)) + " " + z + str((i-4)) + " " + z + str((i-5)) + " " + z + str((i-6)) + " " + z + str((i-7)))
+    
